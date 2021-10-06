b02830eb6bf363e07afa5e519d9510f5e8a51a16
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-06 09:41:35 -0600

    wrote create slug route

--- app/controllers/slug_controller.rb
+++ app/controllers/slug_controller.rb
@@ -1,4 +1,19 @@
 class SlugController < ApplicationController
   def index
   end 
+
+  def create
+    @slug = Slug.new(slug_params)
+    if @slug.save
+      redirect_to root_path, notice: "Sucessfull created Slug"
+    else
+      redirect_to root_path, notice: "Failed to create Slug"
+    end    
+  end
+  
+  private
+  
+  def slug_params
+    params.require(:slug).permit(:url)
+  end
 end
