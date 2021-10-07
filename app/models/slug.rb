dfcab8f3efd55a6b197d3bcca697096c780703ac
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-07 15:06:55 -0600

    add URL validation and unique slug generator before save

--- app/models/slug.rb
+++ app/models/slug.rb
@@ -1,2 +1,11 @@
 class Slug < ApplicationRecord
+  validates :url, uniqueness: true, format: URI::regexp(%w[http https])
+  before_create :set_slug
+
+  def set_slug
+    loop do 
+      self.slug = SecureRandom.uuid[0, 5]
+      break unless Slug.where(slug: slug).exists?
+    end 
+  end 
 end
