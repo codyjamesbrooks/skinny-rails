9d5d90659289dbe48c2ceaed3fb14674f9a64ccb
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-07 13:28:37 -0600

    finsh post requst with existing slug

--- app/controllers/slug_controller.rb
+++ app/controllers/slug_controller.rb
@@ -1,14 +1,29 @@
 class SlugController < ApplicationController
-  def index
+  skip_before_action :verify_authenticity_token
+  def retrive
   end 
 
   def create
-    @slug = Slug.new(slug_params)
-    if @slug.save
-      redirect_to root_path, notice: "Sucessfull created Slug"
-    else
-      redirect_to root_path, notice: "Failed to create Slug"
-    end    
+    render json: { location: request.host }, status: 200
+    # if Slug.find_by( url: slug_params[:url])
+    #   # url exists in db
+    #   respond_with_slug
+    #   respond_to do |format|
+      
+    #   end
+    # else
+    #   # Create a unique slug
+    #   # Save the slug
+    #   # JSON response
+    # end 
+    
+    # redirect_to root_path
+    # # if @slug.save
+
+    # #   redirect_to root_path, notice: "Sucessfull created Slug"
+    # # else
+    # #   redirect_to root_path, notice: "Failed to create Slug"
+    # # end    
   end
   
   private
@@ -16,4 +31,10 @@ class SlugController < ApplicationController
   def slug_params
     params.require(:slug).permit(:url)
   end
+
+  def create_slug( url )
+    url.parameterize[0, 5]
+  end
+
+
 end
