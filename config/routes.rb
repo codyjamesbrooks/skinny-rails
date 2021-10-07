3298bc7e009d78a184f1022b1d45ad9af5edc2d4
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-07 12:14:14 -0600

    redo routes to not have a front end

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'slug#index'
-  post '/', to: 'slug#create'
+  get'/' => 'slug#retrive'
+  post '/' => 'slug#create'
 end
+ 
