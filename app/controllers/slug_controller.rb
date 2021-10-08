121060184121efa0175340b15ee28593ea25316c
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-08 13:57:42 -0600

    wrote get request at existing slug

--- app/controllers/slug_controller.rb
+++ app/controllers/slug_controller.rb
@@ -1,21 +1,37 @@
 class SlugController < ApplicationController
   skip_before_action :verify_authenticity_token
   def retrive
+    @slug = Slug.find_by(slug: params[:slug])
+    if @slug
+      # Create Lookup, send full url in response header
+      Lookup.create({
+        slug_id: @slug.id,
+        ip_address: request.remote_ip,
+        referrer: request.referrer
+      })
+      response.set_header("Location:", @slug.url) 
+      redirect_to @slug.url, status: 301
+    else 
+      render json: { error: "Slug does not exist" }, 
+             status: :not_found
+    end 
   end 
 
   def create
     @slug = Slug.new(url: request_url)
 
     if @slug.save
-      render json: { slug: "#{@slug.slug}", 
-                     'lookup_url': create_lookup_url(@slug.slug) },
+      payload = 
+      render json: { location: "#{request.domain}/#{@slug.slug}" },
                      status: 201
-    elsif @slug.errors == :taken
+    end 
+
+    if @slug.errors == :taken
       correct_slug = Slug.find_by(url: @slug.url).slug
-      render json { 'lookup url': create_lookup_url(correct_slug) },
+      render json: { lookup_url: "#{request.domain}/#{correct_slug}" }.to_json,
                     status: 200
     else
-      render json { error: @slug.errors.full_messages.first },
+      render json: { error: @slug.errors.full_messages.first },
                     status: 400
     end
   end
