4c1529a5d05f68b50315ae954f3a1f42541cdcd9
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-12 11:55:14 -0600

    comment out url validation

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -2,4 +2,5 @@ class Lookup < ApplicationRecord
   belongs_to :slug
   validates :slug, presence: true
   validates :ip_address, presence: true, format: { with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex)}
+  # validates :referrer, format: URI::regexp(%w[http https])
 end
