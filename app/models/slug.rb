c2ab05e8919980039e4035b7b1159362c980c2b8
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-12 11:36:00 -0600

    change before_validation to before_save

--- app/models/slug.rb
+++ app/models/slug.rb
@@ -2,12 +2,12 @@ class Slug < ApplicationRecord
   has_many :lookups, dependent: :destroy
   validates :url, presence: true, uniqueness: true, format: URI::regexp(%w[http https])
   validates :slug, presence: true
-  before_validation :set_slug
+  before_save :set_slug
 
   def set_slug
     loop do 
       self.slug = SecureRandom.uuid[0, 5]
       break unless Slug.where(slug: slug).exists?
     end 
-  end 
+  end
 end
