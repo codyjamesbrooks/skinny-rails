f1640b08b6a4522ebd4ebfb4e3594df99f4f5875
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-13 13:22:21 -0600

    add a NaiveUrlValidator class to the model that leverages a shared UrlValidator class. Set slug before validatrion, rather than before_save

--- app/models/slug.rb
+++ app/models/slug.rb
@@ -1,8 +1,15 @@
 class Slug < ApplicationRecord
+  class NaiveUrlValidator < ActiveModel::Validator
+    def validate(record)
+      record.errors.add(:url, "is not a valid url") unless UrlValidator.validate_url(record.url)
+    end
+  end
+
   has_many :lookups, dependent: :destroy
-  validates :url, presence: true, uniqueness: true, format: URI::regexp(%w[http https])
+  validates :url, presence: true, uniqueness: true
   validates :slug, presence: true
-  before_save :set_slug
+  before_validation :set_slug
+  validates_with NaiveUrlValidator
 
   def set_slug
     loop do 
