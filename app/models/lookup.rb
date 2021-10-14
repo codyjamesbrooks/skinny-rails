e5ae9f799025956d8d27dfa702a6a2e2bf4cce8f
Cody Brooks <codyjamesbrooks@gmail.com>
2021-10-13 19:31:42 -0600

    remove url testing from lookup spec as it is in the URLValidator spec, add required and required test to referrer column

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,6 +1,15 @@
 class Lookup < ApplicationRecord
+  class NiaveLookupValidator < ActiveModel::Validator
+    def validate( record )
+      unless UrlValidator.validate_url(record.referrer)
+        record.errors.add(:referrer, "is not a valid url")
+      end 
+    end 
+  end  
+  
   belongs_to :slug
   validates :slug, presence: true
-  validates :ip_address, presence: true, format: { with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex)}
-  # validates :referrer, format: URI::regexp(%w[http https])
+  validates :referrer, presence: true
+  validates :ip_address, presence: true, format: { with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex), message: "is not a valid ip address"}
+  validates_with NiaveLookupValidator
 end
--- spec/models/lookup_spec.rb
+++ spec/models/lookup_spec.rb
@@ -2,27 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Lookup, type: :model do
   subject { create(:lookup) }
-
+  
   it { expect(subject).to be_valid }
 
   it { should validate_presence_of(:slug) }
   it { should validate_presence_of(:ip_address) }
+  it { should validate_presence_of(:referrer) }
   it { should belong_to(:slug) }
-
-  it "rejects invalid urls as referrers" do
-    ["this-is-not-a-url",
-     "http://garbage",
-     "notaformat://notaformat.com",
-     1234,
-     "user@example.com",
-     "!$?"].each_with_index do |referrer, idx|
-      lookup = build(:lookup, referrer: referrer)
-
-      expect { lookup.valid? }.to be_falsey
-      expect { lookup.errors[:referrer] }.not_to be_empty
-      expect { lookup.errors[:referrer] }.to include("is not a valid uri")
-    end
-  end
 
   it "rejects invalid ip addresses" do
     ["garbage",
@@ -33,9 +19,10 @@ RSpec.describe Lookup, type: :model do
      "!$?"].each_with_index do |referrer, idx|
       lookup = build(:lookup, ip_address: referrer)
 
-      expect { lookup.valid? }.to be_falsey
-      expect { lookup.errors[:ip_address] }.not_to be_empty
-      expect { lookup.errors[:ip_address] }.to include("is not a valid ip address")
+      expect(lookup.valid?).to be_falsey
+      expect(lookup.errors[:ip_address]).not_to be_empty
+      expect(lookup.errors[:ip_address]).to include("is not a valid ip address")
     end
   end
 end
+
